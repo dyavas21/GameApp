7e90178631c2a11fca60c68cb5cddf2a174a0953
GedeD <dyavas21@gmail.com>
2023-02-03 22:11:29 +0700

    component

---
+++ screens/GameScreen.js
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <Text></Text>
+      <View>
+        <Text>Higher or Lower</Text>
+        {/* + - */}
+      </View>
+      {/* <View>Log Rounds</View> */}
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
